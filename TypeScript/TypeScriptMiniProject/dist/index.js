bf520e0ae3feb499e7ea749bce17eef25a48308f
YongJun Kwon <dydwns18011@gmail.com>
2023-03-19 16:35:32 +0900

    feat: 할 일 목록 기능 추가

--- TypeScript/TypeScriptMiniProject/dist/index.js
+++ TypeScript/TypeScriptMiniProject/dist/index.js
@@ -1,2 +1,25 @@
 "use strict";
-console.log("test");
+const todos = [];
+const btn = document.getElementById("btn");
+const input = document.getElementById("todoinput");
+const form = document.querySelector("form");
+const list = document.getElementById("todolist");
+function handleSubmit(e) {
+    e.preventDefault();
+    const newTodo = {
+        text: input.value,
+        completed: false
+    };
+    todos.push(newTodo);
+    createTodo(newTodo);
+    input.value = "";
+}
+function createTodo(todo) {
+    const newLi = document.createElement("li");
+    const checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    newLi.append(todo.text);
+    newLi.append(checkBox);
+    list === null || list === void 0 ? void 0 : list.append(newLi);
+}
+form.addEventListener("submit", handleSubmit);
--- TypeScript/TypeScriptMiniProject/src/index.ts
+++ TypeScript/TypeScriptMiniProject/src/index.ts
@@ -1,1 +1,42 @@
-console.log("test");
+interface Todo {
+    text: string;
+    completed: boolean;
+}
+
+const todos: Todo[] = [];
+
+const btn = document.getElementById("btn")! as HTMLButtonElement;
+const input = document.getElementById("todoinput")! as HTMLInputElement;
+const form = document.querySelector("form")!;
+const list = document.getElementById("todolist");
+
+function handleSubmit(e : SubmitEvent){
+    e.preventDefault();
+    const newTodo: Todo = {
+        text: input.value,
+        completed: false
+    }
+    todos.push(newTodo);
+    createTodo(newTodo);
+   
+
+    input.value = "";
+}
+
+function createTodo(todo: Todo){
+    const newLi = document.createElement("li");
+    const checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    newLi.append(todo.text);
+    newLi.append(checkBox);
+    list?.append(newLi);
+}
+
+form.addEventListener("submit", handleSubmit);
+
+
+
+
+
+
+
