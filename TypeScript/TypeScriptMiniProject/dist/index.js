b5d7499ab7269e5c04b7295aa88203267cc2aeeb
YongJun Kwon <dydwns18011@gmail.com>
2023-03-19 16:49:21 +0900

    feat: 할 일 목록 로컬스토리지 저장 기능 추가

--- TypeScript/TypeScriptMiniProject/dist/index.js
+++ TypeScript/TypeScriptMiniProject/dist/index.js
@@ -1,9 +1,24 @@
 "use strict";
-const todos = [];
 const btn = document.getElementById("btn");
 const input = document.getElementById("todoinput");
 const form = document.querySelector("form");
 const list = document.getElementById("todolist");
+const todos = readTodos();
+todos.forEach(createTodo);
+/**
+ * todo 로컬스토리지에 저장된 값 반환
+ * @returns
+ */
+function readTodos() {
+    const todoListJSON = localStorage.getItem("todos");
+    if (todoListJSON == null)
+        return [];
+    return JSON.parse(todoListJSON);
+}
+/**
+ * click 이벤트
+ * @param e
+ */
 function handleSubmit(e) {
     e.preventDefault();
     const newTodo = {
@@ -12,8 +27,13 @@ function handleSubmit(e) {
     };
     todos.push(newTodo);
     createTodo(newTodo);
+    localStorage.setItem("todos", JSON.stringify(todos));
     input.value = "";
 }
+/**
+ * todo dom 생성
+ * @param todo
+ */
 function createTodo(todo) {
     const newLi = document.createElement("li");
     const checkBox = document.createElement("input");
@@ -22,4 +42,7 @@ function createTodo(todo) {
     newLi.append(checkBox);
     list === null || list === void 0 ? void 0 : list.append(newLi);
 }
+/**
+ * click 이벤트 리스너
+ */
 form.addEventListener("submit", handleSubmit);
--- TypeScript/TypeScriptMiniProject/src/index.ts
+++ TypeScript/TypeScriptMiniProject/src/index.ts
@@ -3,13 +3,28 @@ interface Todo {
     completed: boolean;
 }
 
-const todos: Todo[] = [];
 
 const btn = document.getElementById("btn")! as HTMLButtonElement;
 const input = document.getElementById("todoinput")! as HTMLInputElement;
 const form = document.querySelector("form")!;
 const list = document.getElementById("todolist");
+const todos: Todo[] = readTodos();
+todos.forEach(createTodo);
 
+/**
+ * todo 로컬스토리지에 저장된 값 반환 
+ * @returns 
+ */
+function readTodos(): Todo[]{
+    const todoListJSON = localStorage.getItem("todos");
+    if(todoListJSON == null) return [];
+    return JSON.parse(todoListJSON);
+}
+
+/**
+ * click 이벤트
+ * @param e 
+ */
 function handleSubmit(e : SubmitEvent){
     e.preventDefault();
     const newTodo: Todo = {
@@ -19,10 +34,14 @@ function handleSubmit(e : SubmitEvent){
     todos.push(newTodo);
     createTodo(newTodo);
    
-
+    localStorage.setItem("todos", JSON.stringify(todos));
     input.value = "";
 }
 
+/**
+ * todo dom 생성
+ * @param todo 
+ */
 function createTodo(todo: Todo){
     const newLi = document.createElement("li");
     const checkBox = document.createElement("input");
@@ -32,6 +51,10 @@ function createTodo(todo: Todo){
     list?.append(newLi);
 }
 
+
+/**
+ * click 이벤트 리스너
+ */
 form.addEventListener("submit", handleSubmit);
 
 
