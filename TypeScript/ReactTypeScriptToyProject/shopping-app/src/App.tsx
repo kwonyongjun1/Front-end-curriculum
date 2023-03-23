89f844d1d20e6188beda6e04a1f9a033240d9d85
YongJun Kwon <dydwns18011@gmail.com>
2023-03-23 22:00:12 +0900

    feat: component 작성

--- TypeScript/ReactTypeScriptToyProject/shopping-app/src/App.tsx
+++ TypeScript/ReactTypeScriptToyProject/shopping-app/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Greeter from './components/Greeter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1> Hi </h1>
+      <Greeter></Greeter>
+      <Greeter></Greeter>
+      <Greeter></Greeter>
     </div>
   );
 }
---
+++ TypeScript/ReactTypeScriptToyProject/shopping-app/src/components/Greeter.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Greeter(): JSX.Element{
+    return <h1>Hello!</h1>;
+}
+
+export default Greeter;
