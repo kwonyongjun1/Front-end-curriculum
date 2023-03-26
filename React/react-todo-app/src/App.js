c11998c819254200bbba1f948ed78a1d7531dd49
YongJun Kwon <dydwns18011@gmail.com>
2023-03-26 16:34:19 +0900

    feat: 할 일 목록 UI 작성

--- React/react-todo-app/src/App.js
+++ React/react-todo-app/src/App.js
@@ -2,12 +2,35 @@ import React, {Component}from 'react';
 import './App.css';
 
 export default class App extends Component{
+btnStyle = {
+  color : "#fff",
+  border : "none",
+  padding : "5px 9px",
+  borderRadius : "50%",
+  cursor : "pointer",
+  float : "right"
+}
+
+getStyle = () =>{
+  return {
+    padding : "10px",
+    borderBottom : "1px #ccc dotted",
+    textDecoration : 'none'
+  }
+}
+
   render(){
     return(
       <div className='container'>
         <div className='todoBlock'>
           <div className='title'>
             <h1>할 일 목록</h1>
+          </div>
+
+          <div style={this.getStyle()}>
+            <input type= "checkbox" defaultChecked={false}/>
+            공부하기
+            <button style={this.btnStyle}>X</button>
           </div>
         </div>
       </div>
