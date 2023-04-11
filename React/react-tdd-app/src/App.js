a6d486114b5bad5bda201cbbd322ab111d53b356
YongJun Kwon <dydwns18011@gmail.com>
2023-04-11 21:36:17 +0900

    feat: plus , minus button 테스트 코드 추가

--- React/react-tdd-app/src/App.js
+++ React/react-tdd-app/src/App.js
@@ -7,6 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h3 data-testId="counter">{count}</h3>    
+        <button data-testId = "plus-button" onClick={()=>{setCount((prev) => prev +1)}}>+</button>
+        <button data-testId = "minus-button" onClick={()=>{setCount((prev)=> prev - 1)}}>-</button>
       </header>
     </div>
   );
--- React/react-tdd-app/src/App.test.js
+++ React/react-tdd-app/src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
 test('the counter starts at 0', () => {
@@ -6,3 +6,32 @@ test('the counter starts at 0', () => {
   const counterElement = screen.getByTestId("counter");
   expect(counterElement).toHaveTextContent(0);
 });
+
+
+test('minus button has correct text ', () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("minus-button");
+  expect(buttonElement).toHaveTextContent("-");
+})
+
+test('plus button has correct text', () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("plus-button");
+  expect(buttonElement).toHaveTextContent("+");
+})
+
+test('when the + button is pressed, the counter changes to 1', () => {
+  render(<App/>);
+  const buttonElement = screen.getByTestId("plus-button");
+  fireEvent.click(buttonElement);
+  const coutnerElement = screen.getByTestId("counter");
+  expect(coutnerElement).toHaveTextContent(1);
+})
+
+test('when the - button is pressed, the counter changes to -1', () => {
+  render(<App/>);
+  const buttonElement = screen.getByTestId("minus-button");
+  fireEvent.click(buttonElement);
+  const coutnerElement = screen.getByTestId("counter");
+  expect(coutnerElement).toHaveTextContent(-1);
+})
