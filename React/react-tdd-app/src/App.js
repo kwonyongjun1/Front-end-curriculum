87937453876f3a4b872dce565a81ce4f8fc9c0ac
YongJun Kwon <dydwns18011@gmail.com>
2023-04-11 21:20:15 +0900

    feat: counter 테스트 코드 작성

--- React/react-tdd-app/src/App.js
+++ React/react-tdd-app/src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3 data-testId="counter">{count}</h3>    
       </header>
     </div>
   );
--- React/react-tdd-app/src/App.test.js
+++ React/react-tdd-app/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('the counter starts at 0', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(0);
 });
